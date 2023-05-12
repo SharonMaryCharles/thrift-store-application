a4780df368967c5499e5dfd80f539a514a197f6d
Sharon Mary <116333051+SharonMaryCharles@users.noreply.github.com>
2023-05-12 19:02:47 -0400

    commit changes
    commit changes

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.jpeg";
+import product2 from "./assets/products/2.jpeg";
+import product3 from "./assets/products/3.jpeg";
+import product4 from "./assets/products/4.jpeg";
+import product5 from "./assets/products/5.jpeg";
+import product6 from "./assets/products/6.jpeg";
+import product7 from "./assets/products/7.jpeg";
+import product8 from "./assets/products/8.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Everyone has a Story",
+    price: 25.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "The Host",
+    price: 20.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Will Grayson Will Grayson",
+    price: 23.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "A girl like that",
+    price: 18.99,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Girl Online on Tour",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Paper Towns",
+    price: 22.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Life of Pi",
+    price: 25.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Insurgent",
+    price: 37.0,
+    productImage: product8,
+  },
+];
